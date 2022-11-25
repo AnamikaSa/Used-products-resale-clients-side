38fa33197664a6a03c9b0f3869eb2289d1a15b6e
AnamikaSa <amanika0061@gmail.com>
2022-11-25 20:32:51 +0600

    navbar modified

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,43 +1,52 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../context/AuthProvider';
 
 const Navbar = () => {
+
+    const { user,logOut } = useContext(AuthContext);
+
+    const handleLogOut = () => {
+        logOut()
+            .then(() => { })
+            .catch(err => console.log(err));
+    }
+
+    const menus = <React.Fragment>
+        <li><Link to='/' >Home</Link></li>
+        <li><Link to='/' >Advertise</Link></li>
+        <li><Link to='/' >Categories</Link></li>
+        {user?.uid ?
+            <>
+                <li><Link to="/">Dashboard</Link></li>
+                <li><button onClick={handleLogOut}>Sign out</button></li>
+                <li className='text-secondary font-serif p-5'>{user?.email}</li>
+            </>
+            : <li><Link to="/login">Login</Link></li>}
+    </React.Fragment>
+
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-base-100 flex justify-between">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><Link to='/'>Item 1</Link></li>
-                        <li tabIndex={0}>
-                            <Link to='/' className="justify-between">
-                                Parent
-                                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
-                            </Link>
-                            <ul className="p-2">
-                                <li><Link to='/'>Submenu 1</Link></li>
-                                <li><Link to='/'>Submenu 2</Link></li>
-                            </ul>
-                        </li>
-                        <li><Link to='/'>Item 3</Link></li>
+                    <ul tabIndex={1} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        {menus}
                     </ul>
                 </div>
-                <Link to='/' className="btn btn-ghost normal-case text-xl">DressUp</Link>
+                <Link to="/" className="btn btn-ghost normal-case text-xl">DressUp</Link>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal p-0">
-                    <li><Link to='/' >Home</Link></li>
-                    <li><Link to='/' >Advertise</Link></li>
-                    <li><Link to='/' >Categories</Link></li>
-                    <li><Link to='/login' >LogIn</Link></li>
-                    <li><Link to='/signup' >SignUp</Link></li>
+                    {menus}
                 </ul>
             </div>
-
+            {/* <label htmlFor="dashboard-drawer" tabIndex={2} className="btn btn-ghost lg:hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            </label> */}
         </div>
-
     );
 };
 
