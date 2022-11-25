708bad05faa7b693d39c4eca7ff31a2161de790a
AnamikaSa <amanika0061@gmail.com>
2022-11-25 19:31:23 +0600

    auth added

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -5,16 +5,19 @@ import { Link } from 'react-router-dom';
 
 
 const SignUp = () => {
-    const { register, formState: { errors } } = useForm();
-   
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    
+    const handleSignUp=data=>{
+        console.log(data);
+    }
     
 
     return (
         <div className='h-[800px] flex justify-center items-center'>
             <div className='w-96 p-7'>
                 <h2 className='text-xl text-center'>Sign Up</h2>
-          {/* onSubmit={handleSubmit(handleSignUp)}       */}
-          <form >
+          
+          <form onSubmit={handleSubmit(handleSignUp)}>
                     <div className="form-control w-full max-w-xs">
                         <label className="label"> <span className="label-text">Name</span></label>
                         <input type="text" {...register("name", {
@@ -25,7 +28,7 @@ const SignUp = () => {
                     <div className="form-control w-full max-w-xs">
                         <label className="label"> <span className="label-text">Email</span></label>
                         <input type="email" {...register("email", {
-                            required: true
+                            required: "Email Address is required"
                         })} className="input input-bordered w-full max-w-xs" />
                         {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
                     </div>
@@ -37,7 +40,7 @@ const SignUp = () => {
                         })} className="input input-bordered w-full max-w-xs" />
                         {errors.password && <p className='text-red-500'>{errors.password.message}</p>}
                     </div>
-                    
+                    <input className='btn btn-accent w-full mt-7' value="SignUp" type="submit" />
 
                 </form>
                 <p>Already have an account <Link className='text-secondary' to="/login">Please Login</Link></p>
