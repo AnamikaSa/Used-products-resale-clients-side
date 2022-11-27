90d3022a33e88b49a14106adab93777226659710
AnamikaSa <amanika0061@gmail.com>
2022-11-27 15:55:10 +0600

    modal added

---
+++ src/components/BookingModal.js
@@ -0,0 +1,69 @@
+import React, { useContext, useState } from 'react';
+import { format } from 'date-fns'
+import toast from 'react-hot-toast';
+import { AuthContext } from '../context/AuthProvider';
+
+const BookingModal = ({c, setCategory}) => {
+
+    const { name } = c;
+    const [selectedDate, setSelectedDate] = useState(new Date());
+    const date = format(selectedDate, 'PP');
+    const { user } = useContext(AuthContext);
+
+    const handleBooking = event => {
+        event.preventDefault();
+        const form = event.target;
+        const Username = form.Username.value;
+        const email = form.email.value;
+        const phone = form.phone.value;
+
+        
+        const booking = {
+            bookingDate: date,
+            productName: name,
+            Username: Username,
+            email,
+            phone,
+        }
+        console.log(booking);
+        setCategory(null);
+
+    
+    
+
+
+    }
+    return (
+        <>
+            <input type="checkbox" id="booking-modal" className="modal-toggle" />
+            <div className="modal">
+                <div className="modal-box relative">
+                    <label htmlFor="booking-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+            
+                    <h3 className="text-lg font-bold">{name}</h3>
+                    {/*   */}
+                    <form onSubmit={handleBooking} className='grid grid-cols-1 gap-3 mt-10'>
+                        <input type="text" disabled value={date} className="input w-full input-bordered " />
+                        
+                        <input name="Username" type="text" defaultValue={user?.displayName} placeholder="Your Name" className="input w-full input-bordered" />
+                        <input name="email" type="email" defaultValue={user?.email} disabled placeholder="Email Address" className="input w-full input-bordered" />
+                        <input name="phone" type="text" placeholder="Phone Number" className="input w-full input-bordered" />
+                        <br />
+                        <input className='btn btn-accent w-full' type="submit" value="Submit" />
+                    </form>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default BookingModal;
+
+
+
+
+
+
+
+
+
--- src/components/CategoryProducts.js
+++ src/components/CategoryProducts.js
@@ -1,13 +1,17 @@
-import React, { } from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import BookingModal from './BookingModal';
 
 const CategoryProducts = () => {
     const p = useLoaderData([]);
-    console.log(p);
+    const [c, setCategory]= useState(null);
+    
+    // console.log(p);
     return (
         <div className='grid grid-cols-1 lg:grid-cols-3 gap-72 pl-28 ml-10 mb-5'>
             {
-                p?.products.map(category => <div key={category._id}>
+                p?.products.map(category => <div key={category._id}
+                >
                     <div className="card w-96  bg-base-100 shadow-xl">
                         <figure><img src={category.img} alt="Album" /></figure>
                         <div className="card-body">
@@ -17,14 +21,21 @@ const CategoryProducts = () => {
                             <p className='font-bold'>Original Price:<span className='text-secondary'> {category.Original_Price}</span></p>
 
                             <p className='font-bold'>Resale Price: <span className='text-secondary'>{category.Resale_Price}</span></p>
+                            <p className='font-bold'>Piece:<span className='text-secondary'> {category.Piece}</span></p>
                             <p className='font-bold'>Location: <span className='text-secondary'> {category.location}</span></p>
                             <div className="card-actions justify-end">
-                                <button className="btn btn-primary">Shop It Now</button>
+                                <label htmlFor="booking-modal" className="btn btn-primary" onClick={()=> setCategory(category)}>Shop It Now</label>
                             </div>
                         </div>
                     </div>
                 </div>)
             }
+            {
+               c && 
+                <BookingModal
+                c={c}
+                setCategory={setCategory}
+            ></BookingModal>}
         </div>
     );
 };
